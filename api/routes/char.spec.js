f8c345fd202d5b4b8565ad71a2b87d9bfdd4c839
Sorin Chis <kish.sorin@yahoo.com>
2019-04-11 16:06:08 +0300

    added test for deleting a char from database

--- api/routes/char.spec.js
+++ api/routes/char.spec.js
@@ -10,18 +10,44 @@ describe("chars model", () => {
 
   describe("insert", () => {
     it("insert char into db", async () => {
-      const newChar = await db("chars").insert({
+      await db("chars").insert({
         name: "Pippin",
         special_power: "ddd",
         world: "Tatooene"
       });
-      const secondChar = await db("chars").insert({
+      await db("chars").insert({
         name: "Vader",
         special_power: "force",
         world: "unknow"
       });
       const allTheChars = await db("chars");
       expect(allTheChars).toHaveLength(2);
+    });
+  });
+
+  describe("delete", () => {
+    it("delete succesfully", async () => {
+      const newChar = await db("chars").insert({
+        name: "Walter",
+        special_power: "Cooking",
+        world: "earth"
+      });
+      await db("chars").insert({
+        name: "Thanos",
+        special_power: "titan",
+        world: "somewhere"
+      });
+      await db("chars").insert({
+        name: "Woddy",
+        special_power: "cioc",
+        world: "earth"
+      });
+      await db("chars")
+        .where({ id: 3 })
+        .delete();
+
+      const database = await db("chars");
+      expect(database).toHaveLength(2);
     });
   });
 });
